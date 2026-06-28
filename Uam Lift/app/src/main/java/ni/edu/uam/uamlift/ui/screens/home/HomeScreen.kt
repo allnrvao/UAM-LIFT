d1fcb5888fbba31992d3c156f67663c283a6e9c6
allnrvao <aracuna@uamv.edu.ni>
2026-06-27 21:11:09 -0600

    Fix: Se reparan algunos conflictos pero quedan pendientes mas errores

--- Uam Lift/app/src/main/java/ni/edu/uam/uamlift/ui/screens/home/HomeScreen.kt
+++ Uam Lift/app/src/main/java/ni/edu/uam/uamlift/ui/screens/home/HomeScreen.kt
@@ -9,10 +9,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
